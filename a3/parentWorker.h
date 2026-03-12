01827562ef56a400b2b4e4c9b20294e66d891a1f
Sofia Borodaenko <soph.bor06@gmail.com>
2026-03-12 01:01:17 -0400

    started on the header file

---
+++ a3/parentWorker.h
@@ -0,0 +1,43 @@
+#ifndef PARENTWORKER_H
+#define PARENTWORKER_H
+#define MAX_CHAR 256
+
+typedef struct {
+    int job_id;
+    char filename[MAX_CHAR];
+
+} job_msg;
+
+typedef struct {
+    int job_id;
+    char original_name[MAX_CHAR];
+    char clean_name[MAX_CHAR];
+    char target_name[MAX_CHAR];
+    char *target_path[MAX_CHAR];
+    char category[MAX_CHAR];
+    int lines;
+    int words;
+    long size; 
+
+} result_msg;
+
+// worker functions 
+void create_result(result_msg *result, int job_id, char *original_name, char *clean_name, char *target_name, char *target_path, char *category, int lines, int words, long size);
+char *clean_filename( char *filename);
+char *create_target_path(char *filename);
+char *categorize_file(char *filename);
+int count_lines(char *filename);
+int count_words(char *filename);
+long count_size(char *filename);
+
+bool check_file_name(const char *filename); // can be used to check if the file is valid before sending it to the worker
+
+// parent functions
+void create_job(job_msg *job, const char *filename, int job_id);
+void create_go_directory(char **dir_name);
+void print_summary(char *original_filenames, char *clean_filenames, char *target_paths, char *categorys, int lines, int words, long size);
+
+
+
+
+#endif
